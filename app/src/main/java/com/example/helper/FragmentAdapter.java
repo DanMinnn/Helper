2ed8558559d6c3d9c1e8c6e66128b7833265739d
DANG MINH <minhdangngoc304@gmail.com>
2023-11-14 23:52:14 +0700

    layout sign up

---
+++ app/src/main/java/com/example/helper/FragmentAdapter.java
@@ -0,0 +1,28 @@
+package com.example.helper;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.lifecycle.Lifecycle;
+import androidx.viewpager2.adapter.FragmentStateAdapter;
+
+public class FragmentAdapter extends FragmentStateAdapter {
+
+    public FragmentAdapter(@NonNull FragmentManager fragmentManager, @NonNull Lifecycle lifecycle) {
+        super(fragmentManager, lifecycle);
+    }
+
+    @NonNull
+    @Override
+    public Fragment createFragment(int position) {
+        if(position == 1){
+            return new SignUp_2_Fragment();
+        }
+        return new SignUp_1_Fragment();
+    }
+
+    @Override
+    public int getItemCount() {
+        return 2;
+    }
+}
---
+++ app/src/main/java/com/example/helper/SignUp_1_Fragment.java
@@ -0,0 +1,18 @@
+package com.example.helper;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+public class SignUp_1_Fragment extends Fragment {
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.layout_dangky_1, container, false);
+    }
+}
